be5df1c1ff140edf5c55cfd0f126de93d72600b8
Moritz Tim W <90388353+moritztim@users.noreply.github.com>
2025-09-09 19:50:58 -0400

    add types

---
+++ types/action.d.ts
@@ -0,0 +1,103 @@
+// deno-lint-ignore-file no-explicit-any
+
+type XPCWrappedNative_NoHelper<T> = T & {
+	QueryInterface(): any;
+};
+
+type FilteredMessageHeader = Omit<
+	messenger.messages.MessageHeader,
+	| "external"
+	| "flagged"
+	| "headerMessageId"
+	| "headersOnly"
+	| "id"
+	| "junk"
+	| "junkScore"
+	| "read"
+	| "new"
+	| "size"
+	| "tags"
+> & {
+	QueryInterface(): any;
+	accountKey: string;
+	andFlags(): any;
+	charset: string;
+	dateInSeconds: number;
+	effectiveCharset: string;
+	envAddr: string;
+	envDate: number;
+	expires: number;
+	flags: number;
+	getAuthorCollationKey(): any;
+	getRecipientsCollationKey(): any;
+	getStringProperty(): any;
+	getStringReference(): any;
+	getSubjectCollationKey(): any;
+	getUint32Property(): any;
+	isFlagged: boolean;
+	isKilled: boolean;
+	isRead: boolean;
+	lineCount: number;
+	markFlagged(): any;
+	markHasAttachments(): any;
+	markRead(): any;
+	messageId: messenger.messages.MessageId;
+	messageKey: number;
+	messageSize: number;
+	mime2DecodedAuthor: string;
+	mime2DecodedRecipients: string;
+	mime2DecodedSubject: string;
+	numReferences: number;
+	offlineMessageSize: number;
+	orFlags(): any;
+	priority: number;
+	properties: Array<
+		| "flags"
+		| "sender"
+		| "recipients"
+		| "subject"
+		| "message-id"
+		| "references"
+		| "dateReceived"
+		| "date"
+		| "priority"
+		| "msgCharSet"
+		| "size"
+		| "threadParent"
+		| "msgThreadId"
+		| "ProtoThreadFlags"
+		| "sender_name"
+		| "storeToken"
+		| "offlineMsgSize"
+		| "numLines"
+		| "preview"
+		| "gloda-dirty"
+		| "keywords"
+		| "gloda-id"
+		| string
+	>;
+	rawSubject: string;
+	setReferences(): any;
+	setStringProperty(): any;
+	setUint32Property(): any;
+	storeToken: `${bigint}`;
+	threadId: number;
+	threadParent: number;
+	uidOnServer: number;
+};
+
+type ChromeWindow = object;
+
+type nsIMsgWindow = {
+	closeWindow(): any;
+	domWindow: ChromeWindow;
+	messageWindowDocShell: object;
+	rootDocShell: null | object;
+	statusFeedback: null | object;
+	transactionManager: null | object;
+};
+
+declare const msgHdrs: Array<XPCWrappedNative_NoHelper<FilteredMessageHeader>>;
+declare const Ci: any;
+declare const filterType: number;
+declare const msgWindow: XPCWrappedNative_NoHelper<nsIMsgWindow>;
